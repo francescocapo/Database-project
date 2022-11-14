892909956104d7db04312a4664d8b9ad5d538b25
Martina Serandrei <martina.serandrei@studenti.luiss.it>
2022-11-14 19:53:04 +0100

    mart

--- database.py
+++ database.py
@@ -3,9 +3,11 @@ import numpy as np
 
 dataset = pd.read_csv('Airplane_Crashes_and_Fatalities_Since_1908.csv')
 dataset.head()
-selection = dataset.iloc[3912:, 1:] #take values from 1990 and take all columns except from the first
+selection = dataset.iloc[3196:, 1:] #take values from 198-ò0 and take all columns except from the first
 #print(selection)
-null_val = dataset.isnull().sum()
+null_val = selection.isnull().sum()
+print('null values are:')
 print(null_val)
+print(len(selection))
+print(selection.nunique())
 
-df = dataset.value_counts()
