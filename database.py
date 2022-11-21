954de5e474314f59da032476350fde11dfa11b46
Martina Serandrei <martina.serandrei@studenti.luiss.it>
2022-11-21 17:06:29 +0100

    mart

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 dataset = pd.read_csv('Airplane_Crashes_and_Fatalities_Since_1908.csv')
 dataset.head()
-selection = dataset.iloc[3196:, 1:] #take values from 198-ò0 and take all columns except from the first
+selection = dataset.iloc[3196:, 1:] #take values from 1980-90 and take all columns except from the first
 #print(selection)
 null_val = selection.isnull().sum()
 print('null values are:')
