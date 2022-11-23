169424e4023440b951eaf1b00b9c19105cc707df
francescocapo <francesco.capo@studenti.luiss.it>
2022-11-23 19:32:13 +0100

    fra

--- database.py
+++ database.py
@@ -4,10 +4,7 @@ import pandas as pd
 dataset = pd.read_csv('Airplane_Crashes_and_Fatalities_Since_1908.csv')
 dataset.head()
 selection = dataset.iloc[3196:, 1:] #take values from 1980-90 and take all columns except from the first
-null_val = selection.isnull().sum()
-#print(selection)
-#print('null values are:')
-#print(null_val)
+
 #print(len(selection))
 #print(selection.nunique())
 
@@ -23,16 +20,14 @@ while len(flight_number)!=2072:
     if n not in flight_number:
         flight_number.append(n)
 
-
-
 dup = {x for x in flight_number if flight_number.count(x) >1}
-print(dup)
+#print(dup)
 selection.insert(6,'Flight#', flight_number)
+
+
+
 serial_num = selection.loc[3196:, 'cn/In']
-
-print(serial_num)
-
-
+#print(serial_num)
 dataset = dataset.replace(np.nan,00)
 
 n = random.randint(100, 8000)
@@ -42,3 +37,10 @@ if n not in serial_num:
 serial_num = selection.loc[3196:, 'cn/In']
 serial_num_ = list(serial_num)
 dup2 = {i for i in serial_num_ if serial_num_.count(i) >1}
+
+
+
+null_val = selection.isnull().sum()
+#print(selection)
+#print('null values are:')
+#print(null_val)
