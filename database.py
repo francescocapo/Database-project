d3713e0833037970940c238bacbe9fa3c5a777b2
francescocapo <francesco.capo@studenti.luiss.it>
2022-12-05 12:38:03 +0100

    fra

--- database.py
+++ database.py
@@ -1,23 +1,86 @@
-import numpy as np
+import mysql.connector as mysql
+from mysql.connector import Error
 import pandas as pd
+import numpy as np
 
-dataset = pd.read_csv('Airplane_Crashes_and_Fatalities_Since_1908.csv')
-dataset.head()
-selection = dataset.iloc[3196:]  # take values from 1980-90 and take all columns except from the first
+dataset = pd.read_csv("Airplane_Crashes_and_Fatalities_Since_1908.csv")
 
-print(len(selection))
-print(selection.nunique())
+db_name='airplane_crashes'
+try:
+    mydb = mysql.connect(host='localhost', user='root', password='franci22') # you can add the auth_plugin here too (ref line 26)
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+        mycursor.execute('SHOW DATABASES')
 
-#dup = {x for x in flight_number if flight_number.count(x) > 1}
-# print(dup)
+        result = mycursor.fetchall()
+        print(result)
+        for x in result:
+            if db_name == x[0]:
+                mycursor.execute('DROP DATABASE ' + db_name) # delete old database
+                mydb.commit() # make the changes official
+                print("The database already exists! The old database has been deleted!)")
 
-###working on primary keys
+        mycursor.execute("CREATE DATABASE "+ db_name)
+        print("Database is created")
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+mycursor.execute('USE' + ' ' + db_name)
+
+#CREATE TABLES
+
+mycursor.execute(
+      '''
+        CREATE TABLE `Airplane` (
+          `serial_number` varchar(100),
+          `registration` varchar(100),
+          `operator` varchar(100),
+          `type` varchar(100),
+          PRIMARY KEY (serial_number, registration)
+        );
+      '''
+      )
+
+mycursor.execute(
+      '''
+        CREATE TABLE Flight (
+           flight_number varchar(100),
+           aboard int,
+           route varchar(100),
+           serial_number varchar(100),
+           registration varchar(100),
+           PRIMARY KEY (flight_number),
+           FOREIGN KEY (`serial_number`, `registration`) REFERENCES `Airplane` (`serial_number`, `registration`)
+        );
+      '''
+      )
+
+mycursor.execute(
+    '''
+        CREATE TABLE `Crash` (
+          `index` int,
+          `date` date,
+          `time` time,
+          `location` varchar(100),
+          `serial_number` varchar(100),
+          `registration` varchar(100),
+          `number_of_fatalities` int,
+          `ground` int,
+          `summary` varchar(2000),
+          PRIMARY KEY (`index`),
+          FOREIGN KEY (`serial_number`, `registration`) REFERENCES `Airplane`(`serial_number`, `registration`)
+        );
+    '''
+)
+
+#We manipulate the data
+selection = dataset.iloc[3196:] #take values from 1980-90 and take all columns except from the first
+null_val = selection.isnull().sum()
 selection = selection.drop(labels='Flight #', axis=1)
 
+
 #FLIGHT NUMBER
-
 import random
-
 flight_number = []
 
 while len(flight_number) != 2072:
@@ -27,8 +90,8 @@ while len(flight_number) != 2072:
 
 selection.insert(6, 'Flight#', flight_number)
 
-#SERIAL NUMBER
 
+#SERIAL NUMBER
 serial_num = selection.loc[3196:, 'cn/In']
 serial_num_ = list(serial_num)
 selection = selection.drop(labels='cn/In', axis=1)
@@ -40,11 +103,10 @@ for x in range(len(serial_num_)):
             n = random.randint(100, 8000)
         serial_num_[x] = str(n)
 
-selection.insert(9, 'serial#', serial_num_)
+selection.insert(9, 'Serial#', serial_num_)
 
 
 #REGISTRATION
-
 registration = selection.loc[3196:, 'Registration']
 registration_ = list(registration)
 selection = selection.drop(labels='Registration', axis=1)
@@ -61,7 +123,6 @@ selection.insert(8, 'Registration', registration_)
 
 
 #TIME
-
 import datetime
 from datetime import datetime
 
@@ -82,25 +143,42 @@ for t in times:
             t = t[1:]
         if len(p[2]) > 2:
             t = t[:-1]
-        time = datetime.strptime(t, '%H:%M').time()
-        t2.append(time)
-        
+        try:
+            time = datetime.strptime(t, '%H:%M').time()
+            t2.append(time)
+        except:
+            t2.append(0)
+            continue
 selection = selection.drop(labels='Time', axis=1)
 selection.insert(2, 'Time', t2)
 
 
 #DATE
-
 date = selection.loc[3196:, 'Date']
 date_ = list(date)
 list = [datetime.strptime(x,'%m/%d/%Y').strftime('%Y/%m/%d') for x in date_]
-print(list)
 selection = selection.drop(labels='Date', axis=1)
 selection.insert(1, 'Date', list)
 
+#Insert the data into the tables
 
+#Airplane
+datairplane = selection[['Serial#', 'Registration', 'Operator', 'Type']]
+for i,row in datairplane.iterrows():
+    sql = "INSERT INTO airplane_crashes.Airplane VALUES (%s,%s,%s,%s)"
+    mycursor.execute(sql, tuple([row['Serial#'], row['Registration'], row['Operator'], row['Type']]))
+    mydb.commit()
 
-null_val = selection.isnull().sum()
-#print(selection)
-#print('null values are:')
-print(null_val)
+#Flight
+dataflight = selection[['Flight#', 'Aboard', 'Route', 'Serial#', 'Registration']]
+for i, row in dataflight.iterrows():
+    sql = "INSERT INTO airplane_crashes.Flight VALUES (%s,%s,%s,%s,%s)"
+    mycursor.execute(sql, tuple([row['Flight#'], row['Aboard'], row['Route'], row['Serial#'], row['Registration']]))
+    mydb.commit()
+
+#Crash
+datacrash = selection[['index', 'Date', 'Time', 'Location', 'Serial#', 'Registration', 'Fatalities', 'Ground', 'Summary']]
+for i, row in datacrash.iterrows():
+    sql = "INSERT INTO airplane_crashes.Crash VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    mycursor.execute(sql, tuple([row['index'], row['Date'], row['Time'], row['Location'],  row['Serial#'], row['Registration'],  row['Fatalities'], row['Ground'], row['Summary']]))
+    mydb.commit()
